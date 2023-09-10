fa3c9b51aaddedf4a11b131bbc176807eaf95cc9
JWMatheo <matmatvallone8@gmail.com>
2023-09-10 17:48:11 +0200

    init

---
+++ scripts/setMessage.js
@@ -0,0 +1,30 @@
+const hre = require("hardhat");
+const { encryptDataField } = require("@swisstronik/swisstronik.js");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpclink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpclink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x63f18A704ab3A162DcA7cB35AC01782E85B28e6C";
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "setMessage";
+  const messageToSet = "Custom message";
+  const setMessageTx = await sendShieldedTransaction(signer, contractAddress, contract.interface.encodeFunctionData(functionName, [messageToSet]), 0);
+  await setMessageTx.wait();
+  console.log("Transaction Receipt: ", setMessageTx);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
